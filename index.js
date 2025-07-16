b70f61f143330f55b142182e8a4260ab2198798d
Desmondang62 <desmondang227@gmail.com>
2025-07-16 15:36:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -43,4 +43,5 @@ app.post('/webhook', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
